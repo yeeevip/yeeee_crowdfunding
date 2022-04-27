88dc1420602d3c4fa88c008c7758ac4fb318f25c
qhl <quhailong1995@163.com>
2022-04-27 23:55:59 +0800

    add：init springboot

---
+++ src/main/java/com/yeeee/crowdfunding/YeeeeCrowdfundingApplication.java
@@ -0,0 +1,13 @@
+package com.yeeee.crowdfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeeeeCrowdfundingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeeeeCrowdfundingApplication.class, args);
+    }
+
+}
