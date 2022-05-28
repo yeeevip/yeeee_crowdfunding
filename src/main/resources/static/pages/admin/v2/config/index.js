a310d9e51392feacadfe8fb1f800e3a9373993af
yeah <quhailong1995@163.com>
2022-05-29 00:47:15 +0800

    upd：admin.v2-page

--- src/main/resources/static/pages/admin/v2/config/index.js
+++ src/main/resources/static/pages/admin/v2/config/index.js
@@ -10,7 +10,7 @@
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '2205221823';   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '2205282338';   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
 
